6713fd1cd492c69a9feaedba20892940a45f746f
Azizov1234 <baymirzayevadilnoza0@gmail.com>
2026-04-10 19:11:10 +0500

    fix(group-course): add JwtModule+PrismaModule to GroupCourseModule, exclude prisma from tsconfig.build

--- backend/src/models/group-course/group-course.module.ts
+++ backend/src/models/group-course/group-course.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { GroupCourseService } from './group-course.service';
 import { GroupCourseController } from './group-course.controller';
+import { PrismaModule } from 'src/core/config/prisma.module';
 
 @Module({
+  imports: [PrismaModule, JwtModule],
   controllers: [GroupCourseController],
   providers: [GroupCourseService],
 })
