ab29fb600a2a2f0e223fdf539ade467f7457773e
Azizov1234 <baymirzayevadilnoza0@gmail.com>
2026-04-27 09:38:33 +0500

    Fix LeadModule AuthGuard JwtService dependency

--- backend/src/models/lead/lead.module.ts
+++ backend/src/models/lead/lead.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LeadService } from './lead.service';
 import { LeadController } from './lead.controller';
 import { PrismaModule } from 'src/core/config/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule],
   controllers: [LeadController],
   providers: [LeadService],
   exports: [LeadService],
